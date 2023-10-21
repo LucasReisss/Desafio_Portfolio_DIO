285569c6f898a2249b2987d7f34f5b4aa40201eb
Lucas Reis <lucasbarroreis@gmail.com>
2023-10-21 01:28:14 -0300

    Initial Commit

---
+++ App_List/App_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_ListApp.swift
+//  App_List
+//
+//  Created by Lucas da Silva Reis on 21/10/23.
+//
+
+import SwiftUI
+
+@main
+struct App_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
